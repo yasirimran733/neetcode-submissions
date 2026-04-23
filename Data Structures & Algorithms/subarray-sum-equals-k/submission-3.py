17fb947ce5a49f602ea470eb8b7eff991b8ec47b
Yasir Imran <freelanceryasir786@gmail.com>
2026-04-22 22:39:02 -0400

    Add: subarray-sum-equals-k - submission-3

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subarraySum(self, nums, k):
+        prefixSum = [0] * (len(nums) + 1)
+
+        for i in range(1,len(nums)+1):
+            prefixSum[i] = prefixSum[i-1]+nums[i-1]
+
+        count = 0
+        for i in range(len(nums)):
+            for j in range(i,len(nums)):
+                if(prefixSum[j+1] - prefixSum[i] == k):
+                    count += 1    
+
+        return count    
